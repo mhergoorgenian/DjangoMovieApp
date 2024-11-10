c99eab837e3a7311ff7db1ce64f496c6a0017a41
mhergoorgenian <mher.goor@gmail.com>
2024-11-10 20:54:08 +0330

    commiting all changes

--- myproject/movies/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import AuthorModel,CategoryModel,MovieModel
-# Register your models here.
-admin.site.register(MovieModel)
-admin.site.register(CategoryModel)
-admin.site.register(AuthorModel)
